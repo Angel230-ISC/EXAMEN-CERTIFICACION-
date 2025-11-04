27604d01dafe2d84d0c6bead2100ecd8a632c7ad
Barragan11 <158596646+Barragan11@users.noreply.github.com>
2025-11-04 16:41:06 -0600

    Delete back directory

--- back/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { enviarComentario } = require("../controllers/comentarios.controller");
-
-// Ruta para recibir los comentarios del formulario de contacto
-router.post("/contacto", enviarComentario);
-
-module.exports = router;
--- back/routes/pago.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { realizarPago } = require("../controllers/pago.controller");
-const { verifyToken } = require("../middleware/auth.middleware");
-const router = express.Router();
-
-router.post("/pago", verifyToken, realizarPago);
-
-module.exports = router;
